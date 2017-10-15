3d31293148f0fa6f7017dd250172d4ccb4bdd57b
Sandip Jadhav <sandip.jadhav36@gmail.com>
2017-10-15 21:36:35 +0530

    first commit

---
+++ SimpleProject/Extension/Reachability.swift
@@ -0,0 +1,13 @@
+//
+//  Reachability.swift
+//  SimpleProject
+//
+//  Created by applicationsupport on 15/10/17.
+//  Copyright © 2017 SandipJadhav. All rights reserved.
+//
+
+import UIKit
+
+class Reachability: NSObject {
+
+}
